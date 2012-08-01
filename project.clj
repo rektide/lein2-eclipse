98ab71850d2d7ad8d743fa9a770ae6a02c95a40b
Tom Denley <tom.denley@youdevise.com>
2012-08-01 12:50:37 +0100

    preview release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein2-eclipse "2.0.0"
+(defproject lein2-eclipse "2.0.0-preview1"
   :description "A leiningen plugin to create Eclipse project descriptor files."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [prxml "1.3.1"]
