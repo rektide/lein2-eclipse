ce58c1be08b3f6b111fcd688bda8fc70ded3b0b2
Tom Denley <tom.denley@youdevise.com>
2012-08-01 12:59:41 +0100

    license and url

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject lein2-eclipse "2.0.0-preview1"
   :description "A leiningen plugin to create Eclipse project descriptor files."
+  :url "http://netmelody.org/lein2-eclipse/"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [prxml "1.3.1"]
                  [leiningen-core "2.0.0-preview7"]
