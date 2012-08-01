a82abd10b3f156261b6c7797b549a9e9e988e601
Tom Denley <tom.denley@youdevise.com>
2012-08-01 12:42:16 +0100

    update to lein2-eclipse

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-eclipse "2.0.0"
+(defproject lein2-eclipse "2.0.0"
   :description "A leiningen plugin to create Eclipse project descriptor files."
-  :dependencies [[org.clojure/clojure "1.3.0"]
-                     [prxml "1.3.1"]
-                     [leiningen-core "2.0.0-preview4"]
-                     [lein-clojars "0.8.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [prxml "1.3.1"]
+                 [leiningen-core "2.0.0-preview7"]
+                 [lein-clojars "0.9.0"]])
